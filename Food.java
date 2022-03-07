d9491d0ab447cb3f2bcc51bd41d70dffaff1bec5
PierreLs63 <pierrelouis.vidal@hotmail.fr>
2022-03-07 08:55:19 +0100

    Update Food.java

--- Food.java
+++ Food.java
@@ -1,3 +1,21 @@
+import java.awt.*;
+
 public class Food {
-    private String test;
+    public double pos_x;
+    public double pos_y;
+    public double size;
+    public Food(double x,double y,double size){
+        this.size=size;
+        pos_x=x;
+        pos_y=y;
+    }
+    public Food(double x,double y){
+        size=5.0;
+        pos_x=x;
+        pos_y=y;
+    }
+    public void draw (Graphics g, Color c){ //dessine la nourriture
+        g.setColor(c);
+        g.fillOval((int)(pos_x), (int)(pos_y), (int)size, (int)size);
+    }
 }
