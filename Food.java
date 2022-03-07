22a5f6640381fe04f54ac7f87e52ae2bf463bed6
Shaarllun <78822946+Shaarllun@users.noreply.github.com>
2022-03-07 09:27:38 +0100

    mise en forme food

--- Food.java
+++ Food.java
@@ -4,18 +4,21 @@ public class Food {
     public double pos_x;
     public double pos_y;
     public double size;
-    public Food(double x,double y,double size){
-        this.size=size;
-        pos_x=x;
-        pos_y=y;
+
+    public Food(double x, double y, double size) {
+        this.size = size;
+        pos_x = x;
+        pos_y = y;
     }
-    public Food(double x,double y){
-        size=5.0;
-        pos_x=x;
-        pos_y=y;
+
+    public Food(double x, double y) {
+        size = 5.0;
+        pos_x = x;
+        pos_y = y;
     }
-    public void draw (Graphics g, Color c){ //dessine la nourriture
+
+    public void draw(Graphics g, Color c) { // dessine la nourriture
         g.setColor(c);
-        g.fillOval((int)(pos_x), (int)(pos_y), (int)size, (int)size);
+        g.fillOval((int) (pos_x), (int) (pos_y), (int) size, (int) size);
     }
 }
