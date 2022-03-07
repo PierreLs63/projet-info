8f406f3f3701731c709daeb1f668d4507d813c45
Shaarllun <78822946+Shaarllun@users.noreply.github.com>
2022-03-07 09:27:02 +0100

    mise en forme blob

--- Blob.java
+++ Blob.java
@@ -1,60 +1,58 @@
 
 import java.awt.*;
 
-
 public class Blob {
-    //Caractéristiques du blob
+    // Caractéristiques du blob
     public double speed;
     public double size;
     public double view_range;
     public double energy = 1000;
     public boolean alive = true;
-    //position du blob
+    // position du blob
     public double pos_x = 0;
     public double pos_y = 0;
     public boolean wallBounce = false;;
 
     // liste des vecteurs vitesse
-    public Vect speedV = new Vect (0,0);
-    public Vect newSpeedV= new Vect (0,0);
-    public Vect wanderingForceV= new Vect (0,0);
-    
+    public Vect speedV = new Vect(0, 0);
+    public Vect newSpeedV = new Vect(0, 0);
+    public Vect wanderingForceV = new Vect(0, 0);
+
     // parametres de deplacement
     public double wanderingStrength = 0;
     public double orientation = 0;
     public double steeringStrength = 20;
-   
 
-    public Blob (double sp, double si, double vr){
+    public Blob(double sp, double si, double vr) {
         speed = sp;
         size = si;
         view_range = vr;
     }
 
-    public void VectSpeed(){ //calcul la nouvelle direction et vitesse du blob
-        movement(); 
+    public void VectSpeed() { // calcul la nouvelle direction et vitesse du blob
+        movement();
         computeNewVitesse();
         speedV.x = newSpeedV.x;
         speedV.y = newSpeedV.y;
         orientation = speedV.angle();
-        
+
     }
 
-       public void VectSpeed(Vect newForce, int newForceStrength){ //calcul la nouvelle direction et vitesse du blob
-        movement(); 
+    public void VectSpeed(Vect newForce, int newForceStrength) { // calcul la nouvelle direction et vitesse du blob
+        movement();
         computeNewVitesse(newForce, newForceStrength);
         speedV.x = newSpeedV.x;
         speedV.y = newSpeedV.y;
         orientation = speedV.angle();
-        
+
     }
 
-    public void movement (){ //fait se déplacer le blob
-        pos_x +=  speedV.x;
-        pos_y +=  speedV.y;
+    public void movement() { // fait se déplacer le blob
+        pos_x += speedV.x;
+        pos_y += speedV.y;
     }
 
-    public void computeNewVitesse() { //calcul la nouvelle vitesse du blob
+    public void computeNewVitesse() { // calcul la nouvelle vitesse du blob
         computeWanderingForce();
         newSpeedV = (speedV.times(steeringStrength)).vectAdd(wanderingForceV.times(wanderingStrength));
         newSpeedV.normalize();
@@ -64,31 +62,25 @@ public class Blob {
 
     public void computeNewVitesse(Vect newForce, int newForceStrength) {
         computeWanderingForce();
-        newSpeedV = (speedV.times(steeringStrength)).vectAdd(wanderingForceV.times(wanderingStrength).vectAdd(newForce.times(newForceStrength)));
+        newSpeedV = (speedV.times(steeringStrength))
+                .vectAdd(wanderingForceV.times(wanderingStrength).vectAdd(newForce.times(newForceStrength)));
         newSpeedV.normalize();
         newSpeedV = newSpeedV.times(speed);
 
     }
 
-    public void computeWanderingForce() { //calcul la force aléatoire qui s'aplique au blob pour le faire se déplacer aléatoirement dans l'espace
-        orientation += Math.PI/2;
-        wanderingForceV.x += (Math.random()-0.5) * Math.cos(orientation);
-        wanderingForceV.y += (Math.random()-0.5) * Math.sin(orientation);
+    public void computeWanderingForce() { // calcul la force aléatoire qui s'aplique au blob pour le faire se déplacer
+                                          // aléatoirement dans l'espace
+        orientation += Math.PI / 2;
+        wanderingForceV.x += (Math.random() - 0.5) * Math.cos(orientation);
+        wanderingForceV.y += (Math.random() - 0.5) * Math.sin(orientation);
         wanderingForceV.normalize();
 
     }
 
-    public void draw (Graphics g, Color c){ //dessine un blob
+    public void draw(Graphics g, Color c) { // dessine un blob
         g.setColor(c);
-        g.fillOval((int)(pos_x), (int)(pos_y), (int)size, (int)size);
+        g.fillOval((int) (pos_x), (int) (pos_y), (int) size, (int) size);
     }
 
-   
-
 }
-
-
-
-
-
-
