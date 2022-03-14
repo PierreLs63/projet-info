e90355dd56b2a273d12de1e8759edb19e20d3a9e
PierreLs63 <pierrelouis.vidal@hotmail.fr>
2022-03-14 09:50:39 +0100

    food

--- Food.java
+++ Food.java
@@ -4,6 +4,7 @@ public class Food {
     public double pos_x;
     public double pos_y;
     public double size;
+    public Color color = new Color(68,151,0);
 
     public Food(double x, double y, double size) {
         this.size = size;
@@ -17,8 +18,8 @@ public class Food {
         pos_y = y;
     }
 
-    public void draw(Graphics g, Color c) { // dessine la nourriture
-        g.setColor(c);
+    public void draw(Graphics g) { // dessine la nourriture
+        g.setColor(color);
         g.fillOval((int) (pos_x), (int) (pos_y), (int) size, (int) size);
     }
 }
