f75b80e11adeab08ecce9d148ddb692b2558ede7
Shaarllun <78822946+Shaarllun@users.noreply.github.com>
2022-04-03 21:38:05 +0200

    C'est bon j'ai plus de pb

--- App.java
+++ App.java
@@ -14,52 +14,37 @@ public class App extends JFrame implements ActionListener {
     // map
     public Map map;
     JPanel mapBounds;
-    JPanel affichageMap;
 
     public App (Map aMap){
-        this.setTitle("Le jeu des blobs");
-        this.setResizable(true);
-
         map = aMap;
 
         setBounds(0, 0, 1920, 1000);
+        setTitle("Les Blobs c'est cool");
         setLayout(null);
 
-<<<<<<< HEAD
         JPanel contentPane = new JPanel();
         contentPane.setBounds(0,0,getWidth(),getHeight());
         contentPane.setLayout(null);
 
-        JPanel affichageGlobal = new JPanel();
-        affichageGlobal.setBounds(0,50,1000,1000);
-        affichageGlobal.setBackground(Color.white);
-        affichageGlobal.setLayout(null);
-=======
         JPanel affichageMap = new JPanel();
-        affichageMap.setBounds(0,50,1000,500);
-        affichageMap.setBackground(Color.green);
+        affichageMap.setBounds(0,50,1000,1000);
+        affichageMap.setBackground(Color.white);
         affichageMap.setLayout(null);
->>>>>>> 17a671158c8a085ef2139e0c084f5153b56389b1
 
         mapBounds = new JPanel();
         mapBounds.setBounds(500, 100, map.width, map.height);
         mapBounds.setLayout(null);
-        mapBounds.setBackground(Color.yellow);
+        mapBounds.setBackground(Color.green);
 
         JButton f = new JButton("appuyez");
         f.setBounds(500,500,50,50);
 
 
-        affichageGlobal.add(map);
-        contentPane.add(affichageGlobal);
+        affichageMap.add(map);
+        contentPane.add(affichageMap);
         contentPane.add(f);
 
-<<<<<<< HEAD
         setContentPane(contentPane);
-=======
-        affichageMap.add(mapBounds);
-        add(affichageMap);
->>>>>>> 17a671158c8a085ef2139e0c084f5153b56389b1
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
@@ -70,7 +55,7 @@ public class App extends JFrame implements ActionListener {
         timer.start();
 
     }
-    
+
 
     public void actionPerformed(java.awt.event.ActionEvent e) { // tout ce qui se passe chaque x ms
         if (e.getSource() == timer) {
@@ -100,3 +85,5 @@ public class App extends JFrame implements ActionListener {
 
 
 }
+
+
